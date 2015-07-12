7578463b7e09039a19fa560b9ef4e7a977c58d00
Nick Kuhn <nickuhn@gmail.comn>
2015-07-11 17:50:51 -0700

    Refactored transform.js, fixed gulpfile and bufferReader
    transform.js now is an object with methods to call instead of a switch statement.
    gulpfile now properly watches all necessary files.
    bufferReader updated to use new transform.js, fixed nonpalleted grayscale function.

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ var jshint = require('gulp-jshint');
 gulp.task(('default'), ['test', 'lint', 'watch'], function() {});
 
 gulp.task('test', function(){
-  return gulp.src('test/*.js')
+  return gulp.src('test/*.js, bin/*js')
              .pipe(mocha({ reporter: 'nyan' }));
 });
 
 gulp.task('lint', function(){
-  return gulp.src(['test/*js', '*.js'])
+  return gulp.src(['test/*js', 'bin/*.js'])
              .pipe(jshint())
              .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('watch', function(){
-  return gulp.watch(['test/*js', '*.js'], ['test', 'lint']);
+  return gulp.watch(['test/*js', 'bin/*.js'], ['test', 'lint']);
 });
