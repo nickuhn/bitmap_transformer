d5c02bb9ad3c49db604b25311ed41b46bce17cc3
Nick Kuhn <nickuhn@gmail.comn>
2015-07-12 13:44:08 -0700

    Refactored BufferReader
    Refactored BufferReader to be an object constructor with methods to call transform.js
    Moved logic to index.js and adjusted formatting.

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-var transform = require('./lib/transform');
-var bufferReader = require('./lib/bufferReader');
-var fs = require('fs');
-var os = require('os');
+var fs           = require('fs');
+var BufferReader = require('./lib/bufferReader');
 
 
 fs.readFile(__dirname + process.argv[2], function(err, data){
   if (err) {
     return err;
   }
-  bufferReader('/..' + process.argv[2], process.argv[3], data);
-});
 
-// bufferReader('/../bitmaps/nonpalettebitmap.bmp', 'b');
+  var currentBuffer = new BufferReader('/..' + process.argv[2], process.argv[3], data);
+
+  if (currentBuffer.type === 'BM') {
+    if(currentBuffer.endianness === 'LE') {
+      currentBuffer.offset = currentBuffer.buffer.readUInt32LE(10);
+    } else {
+      currentBuffer.offset = currentBuffer.buffer.readUInt32BE(10);
+    }
+    currentBuffer.headerSize = 54;
+  } else {
+    return 'Please use BM format bitmaps';
+  }
+
+  if (currentBuffer.offset !== currentBuffer.headerSize) {
+    currentBuffer.palette = true;
+  }
+
+  if (currentBuffer.palette) {
+    if (currentBuffer.flag.toLowerCase() === 'gy') {
+      currentBuffer.paletteGrayscaler();
+    } else {
+      currentBuffer.paletteTransformer();
+    }
+    currentBuffer.writeBufferToFile();
+  }
+
+  if (!currentBuffer.palette) {
+    if (currentBuffer.flag.toLowerCase() === 'gy') {
+      currentBuffer.nonPaletteGrayscaler();
+    } else {
+      currentBuffer.nonPaletteTransformer();
+    }
+    currentBuffer.writeBufferToFile();
+  }
+});
