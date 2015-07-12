ceb8c94d319e3e01675955b41641e221fbf18a93
Nick Kuhn <nickuhn@gmail.comn>
2015-07-11 18:25:48 -0700

    more whitespace changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var transform = require('./lib/transform');
 var bufferReader = require('./lib/bufferReader');
+var fs = require('fs');
 
-bufferReader('/../bitmaps/nonpalettebitmap.bmp', 'gy');
+
+bufferReader('/../bitmaps/nonpalettebitmap.bmp', 'b');
 
 //todo add in check for BE versus LE, add in command line interface, rewrite/add tests
