2e1734fff13ff7175fc254aae02129fa09ee08ff
Nick Kuhn <nickuhn@gmail.comn>
2015-07-11 18:04:44 -0700

    Fixed minor code formatting.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var transform = require('./lib/transform');
 var bufferReader = require('./lib/bufferReader');
 
-bufferReader('/../bitmaps/palettebitmap.bmp', 'gy');
+bufferReader('/../bitmaps/nonpalettebitmap.bmp', 'gy');
 
 //todo add in check for BE versus LE, add in command line interface, rewrite/add tests
